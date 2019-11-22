86f3cabe69c9127606a3ed1160ba231a945e9492
Den <den@work>
2019-11-22 12:12:14 +0200

    - added check for certs in /etc/ssl - rewrote logic, stop/start daemons once perl whole work and only if there are expired certs - added stop/start docker containers that publish 80/tcp and 443/tcp ports - added instruction to restart docker containers that have '/etc/ssl' volume - cleanup ACME job from root crontab

--- acme-check.pl
+++ acme-check.pl
@@ -11,66 +11,62 @@ if ($debug ne 'debug' && $debug =~ /\./) {
 	$force_host = $debug;
 }
 
-if (not -e "/root/.acme.sh") {
-	system("curl https://get.acme.sh | sh");
-}
-
 sub parse_nginx {
-    my ($l, $file) = @_;
+	my ($l, $file) = @_;
 
-    if ($debug ne '') {
-    	for(my $i=0;$i<=$l;$i++) { print " "; }
-	print "$file\n";
-    }
+	if ($debug ne '') {
+		for(my $i=0;$i<=$l;$i++) { print " "; }
+		print "$file\n";
+	}
 
-    return if defined $files{$file};
-    $files{$file} = 1;
-    $l++;
+	return if defined $files{$file};
+	$files{$file} = 1;
+	$l++;
 
-    if ($file =~ /\*/) {
-        while(< $file >) {
-            parse_nginx($l, $_);
-        }
-    } else {
-	return if not -e $file;
-	my @raw = ('');
-        open my $fh, '<'. $file;
-	if ($fh) {
-		eval {
-			#print "$file\n";
-			@raw = <$fh>;
-			close $fh;
+	if ($file =~ /\*/) {
+		while(< $file >) {
+			parse_nginx($l, $_);
+		}
+	} else {
+		return if not -e $file;
+		my @raw = ('');
+		open my $fh, '<'. $file;
+		if ($fh) {
+			eval {
+				#print "$file\n";
+				@raw = <$fh>;
+				close $fh;
+			}
+		}
+
+		my($key, $crt) = ('', '');
+		foreach $_ (@raw) {
+			next if /^#/;
+
+			if (/server\s*{/) {
+				($key, $crt) = ('', '');
+			}
+
+			if (/ssl_certificate\s+(\S+)\;/) {
+				$crt = $1;
+			}
+
+			if (/ssl_certificate_key\s+(\S+)\;/) {
+				$key = $1;
+			}
+
+			if ($key ne '' && $crt ne '') {
+				print "- SSL [$crt] [$key]\n" if $debug ne '';
+				$ssl{$crt} = $key;
+				($key, $crt) = ('', '');
+			}
+
+			if (/include\s+([\w\W\d\D\S]+)\;/) {
+				parse_nginx($l, $1);
+			}
 		}
 	}
-	
-        my($key, $crt) = ('', '');
-        foreach $_ (@raw) {
-            if (/server\s*{/) {
-                ($key, $crt) = ('', '');
-            }
-
-            if (/ssl_certificate\s+(\S+)\;/) {
-                $crt = $1;
-				$crt =~ s/[\"\']+//g;
-            }
-
-            if (/ssl_certificate_key\s+(\S+)\;/) {
-                $key = $1;
-				$key =~ s/[\"\']+//g;
-            }
-
-            if ($key ne '' && $crt ne '') {
-                print "- SSL [$crt] [$key]\n" if $debug ne '';
-                $ssl{$crt} = $key;
-                ($key, $crt) = ('', '');
-            }
-
-            if (/include\s+([\w\W\d\D\S]+)\;/) {
-                parse_nginx($l, $1);
-            }
-        }
-    }
-    return;
+	return;
 }
 
 sub check_cert_expire {
@@ -85,40 +81,54 @@ sub check_cert_expire {
 	}
 
 	open my $io, "openssl x509 -in $crt -noout -subject -issuer -checkend 777600 && echo OK || echo FAIL|";
-    my ($exp, $cn, $issuer) = ('y', '', '');
-    while(<$io>) {
-        print "$_\n" if $debug ne '';
-        $exp = 'n' if /OK/;
-        if (/subject/i && /CN\s*\=\s*([\w\d\.\-]+)/) {
-            $cn = $1;
-        }
+	my ($exp, $cn, $issuer) = ('y', '', '');
+	while(<$io>) {
+		print "$_\n" if $debug ne '';
+		$exp = 'n' if /OK/;
+		if (/subject/i && /CN\s*\=\s*([\w\d\.\-]+)/) {
+			$cn = $1;
+		}
 
-        if (/issuer/i && /O\s*\=\s*([^\/]+)/) {
-            $issuer = $1;
-        }
-    }
+		if (/issuer/i && /O\s*\=\s*([^\/]+)/) {
+			$issuer = $1;
+		}
+	}
 	close $io;
+
+	return ('n', '', '') if $cn eq '' || $cn eq 'localhost' || $cn !~ /[\w\d\-]+\.[\w\d]+/;
+
 	return($exp, $cn, $issuer);
 }
 
 parse_nginx 1, '/etc/nginx/nginx.conf' if -e "/etc/nginx/nginx.conf"; # NGINX
-parse_nginx 1, '/usr/local/vesta/nginx/conf/nginx.conf' if -e '/usr/local/vesta/nginx/conf/nginx.conf'; # VESTA
+parse_nginx 1, '/srv/nginx/conf.d/*' if -e "/srv/nginx/conf.d"; # NGINX DOCKER
+parse_nginx 1, '/srv/nginx/data/conf.d/*' if -e "/srv/nginx/data/conf.d"; # NGINX DOCKER
+parse_nginx 1, '/usr/local/vesta/nginx/conf/nginx.conf' if -e '/usr/local/vesta/nginx/conf/nginx.conf'; # NGINX VESTA
 
 # OpenVZ
 if (-e '/opt/ovz-web-panel/config/certs/server.crt' && -e '/opt/ovz-web-panel/config/certs/server.key') {
-    $ssl{'/opt/ovz-web-panel/config/certs/server.crt'} = '/opt/ovz-web-panel/config/certs/server.key';
+	$ssl{'/opt/ovz-web-panel/config/certs/server.crt'} = '/opt/ovz-web-panel/config/certs/server.key';
+}
+
+# Check SSL directory
+while(</etc/ssl/*.crt>) {
+	my $crt = $_;
+	my $key = $_; $key =~ s/\.crt$/.key/;
+	if (-f $key) {
+		$ssl{$crt} = $key;
+	}
 }
 
 # Custom
 if (-e '/etc/ssl/custom.conf') {
-    open FF, '</etc/ssl/custom.conf'; 
-    while(<FF>) {
-	if (/(\S+)\s+(\S+)/) {
-	    print "custom: crt[$1] -> key[$2]\n" if $debug ne '';
-	    $ssl{$1} = $2;
+	open FF, '</etc/ssl/custom.conf'; 
+	while(<FF>) {
+		if (/(\S+)\s+(\S+)/) {
+			print "custom: crt[$1] -> key[$2]\n" if $debug ne '';
+			$ssl{$1} = $2;
+		}
 	}
-    }
-    close FF;
+	close FF;
 } else {
 	if ($debug ne '') {
 		print "custom format:\ncat /etc/ssl/custom.conf\ncrt key\n\n";
@@ -131,32 +141,82 @@ if ($force_host ne '') {
 	$ssl{"/etc/ssl/$force_host.crt"} = "/etc/ssl/$force_host.key";
 }
 
+my @exp;
 foreach my $crt (keys %ssl) {
-    my $key = $ssl{$crt};
-    if ($debug ne '') {
-	    print "\tcrt = [$crt]\n\tkey = [$key]\n\n";
-    }
+	my $key = $ssl{$crt};
+	if ($debug ne '') {
+		print "\tcrt = [$crt]\n\tkey = [$key]\n\n";
+	}
 
-    my ($exp, $cn, $issuer) = check_cert_expire($crt);
-    print "EXP [$exp] CN[$cn] ISSUER[$issuer]\n" if $debug ne '';
+	my ($exp, $cn, $issuer) = check_cert_expire($crt);
+	print "EXP [$exp] CN[$cn] ISSUER[$issuer]\n" if $debug ne '';
 
-    if ($exp eq 'y' && $issuer =~ /let's encrypt/i) {
-        print "EXP $cn\n";
-
-        my @prog;
-        push @prog, 'nginx' if -e '/etc/init.d/nginx';
-		push @prog, 'exim' if -e '/etc/init.d/exim';
-		push @prog, 'dovecot' if -e '/etc/init.d/dovecot';
-		push @prog, 'pveproxy' if -e '/etc/init.d/pveproxy';
-        push @prog, 'vesta' if $crt =~ /\/vesta\//;
-        push @prog, 'owp' if $crt =~ /\/ovz-web-panel\//;
-
-        foreach my $p (@prog) { system("/etc/init.d/$p stop || service $p stop"); }
-
-    	system("cd .acme.sh; ( ./acme.sh --issue -k 4096 --standalone -d $cn --force || ./acme.sh --issue -k 4096 --standalone -d $cn --force --httpport 63219 ) &&
-        		cat /root/.acme.sh/$cn/$cn.key > $key &&
-	        	cat /root/.acme.sh/$cn/fullchain.cer > $crt && echo OK $cn");
-
-        foreach my $p (@prog) { system("/etc/init.d/$p start || service $p start"); }
-    }
+	if ($exp eq 'y' && $issuer =~ /let's encrypt/i) {
+		#print "EXP $cn\n";
+		push @exp, [$cn,$crt,$key];
+	}
 }
+
+if ($#exp eq -1) {
+	print "- nothing to renew, exiting\n";
+	exit 0;
+}
+
+print "- updating ACME\n";
+system("rm -rf /root/.acme.sh") if -e "/root/.acme.sh";
+system("curl https://get.acme.sh | sh");
+
+print "- cleanup ACME from root crontab\n";
+system("crontab -u root -l|grep -v '/root/.acme.sh'|crontab -u root -");
+
+my @prog;
+push @prog, 'nginx' if -e '/etc/init.d/nginx';
+push @prog, 'exim' if -e '/etc/init.d/exim';
+push @prog, 'dovecot' if -e '/etc/init.d/dovecot';
+push @prog, 'pveproxy' if -e '/etc/init.d/pveproxy';
+push @prog, 'vesta' if -e '/usr/local/vesta';
+push @prog, 'owp' if -e '/opt/ovz-web-panel';
+foreach my $p (@prog) { system("/etc/init.d/$p stop || service $p stop") }
+
+my @dockers;
+if (-e '/usr/bin/docker') {
+	open IO, 'docker ps --format {{.ID}} --filter publish=443/tcp --filter publish=80/tcp|';
+	while(<IO>) {
+		if (/([0-9a-f]{12})/) {
+			my $id = $_;
+			push @dockers, $id;
+			print "- stoping docker $id\n";
+			system("docker stop $id");
+		}
+	}
+	close IO;
+}
+
+
+foreach my $x (@exp) {
+	my ($cn, $crt, $key) = @{$x};
+	print "-> RENEW: $cn\n";
+
+	system("cd .acme.sh && \
+		( ./acme.sh --issue -k 4096 --standalone -d $cn --force || ./acme.sh --issue -k 4096 --standalone -d $cn --force --httpport 63219 ) && \
+		cat /root/.acme.sh/$cn/$cn.key > $key && \
+		cat /root/.acme.sh/$cn/fullchain.cer > $crt && \
+		echo OK $cn");
+}
+
+foreach my $p (@prog) { system("/etc/init.d/$p start || service $p start") }
+foreach my $id (@dockers) { print "- starting docker $id\n"; system("docker start $id") }
+
+# restart docker containters that have '/etc/ssl' volumes:
+open IO, 'docker ps --filter volume=/etc/ssl --format {{.ID}}|';
+while(<IO>) {
+	if (/([0-9a-f]{12})/) {
+		my $id = $_;
+		my @f = grep /$id/, @dockers;
+		if ($#f eq -1) {
+			print "- restarting docker $id (has /etc/ssl volume)\n";
+			system("docker restart $id");
+		}
+	}
+}
+close IO;
